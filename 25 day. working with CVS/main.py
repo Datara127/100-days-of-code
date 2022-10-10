3581a66ff7dc2e8c91aedf43170fa1fcf005db53
datara <alexeylos2002@mail.ru>
2022-10-10 09:56:21 +0300

    26 day, nato alphabet

--- 25 day. working with CVS/main.py
+++ 25 day. working with CVS/main.py
@@ -17,10 +17,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{count_right_answer}/50 States correct",
                                     prompt="What's another state's name?")
     if answer_state.lower() == "exit":
-        missing_state = []
-        for state in states:
-            if state not in guessed_states:
-                missing_state.append(state)
+        missing_state = [state for state in states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_state)
         new_data.to_csv("state_to_learn.csv")
         break
